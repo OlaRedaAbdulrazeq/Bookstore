9bc61593303d60f4a6cc62538d600982616b2a6f
OlaRedaAbdulrazeq <olaabdelrazeq@std.mans.edu.eg>
2026-02-19 14:15:03 +0200

    added featured-book logic inside index.js

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+////////////////////////Featured book logic//////////////////////////////
+let quantity = 1;
+    let price = 45;
+    let descriptionVisible = true;
+//function to show description using view more button
+    function toggleDescription() {
+        const desc = document.getElementById("description");
+        descriptionVisible = !descriptionVisible;
+        desc.style.display = descriptionVisible ? "block" : "none";
+    }
+//function to change the quantity like increase and decrease
+    function changeQty(value) {
+        quantity += value;
+        if (quantity < 1) quantity = 1;
+        document.getElementById("quantity").innerText = quantity;
+        updateTotal();
+    }
+//function to calculate  total price after increasing quantity
+    function updateTotal() {
+        let total = quantity * price;
+        document.getElementById("total").innerText = total;
+    }
+//function to add to cart and disaplys book added to cart
+    function addToCart() {
+        const alertBox = document.getElementById("alertBox");
+        alertBox.classList.remove("d-none");
 
-
-
-
-
-
+        setTimeout(() => {
+            alertBox.classList.add("d-none");
+        }, 2000);
+    }
+//function to make start display ur rating
+    function rate(stars) {
+        document.getElementById("rating-text").innerText = stars + " / 5";
+    }
 
 //////////////////////// blog page logic///////////////////////////////////
 // function to display it dynmically
